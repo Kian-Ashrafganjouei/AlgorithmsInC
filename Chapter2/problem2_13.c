e56689bf0def6898f2b130a8dd937f423465d17f
Kian <kian.ashrafganjouei@gmail.com>
2023-07-23 20:27:13 -0400

    Completed Chapter 4 problems in chapter two

---
+++ Chapter2/problem2_13.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int power(int a,int b) {
+	int power = 1;
+
+	for(int i=1; i<=b; i++) power*=a;
+
+	return(power);
+	
+}
+
+int main(void) {
+	int n=10000, ceil, i;
+
+	for(i=0; n>power(2, power(2, i)); i++);
+	ceil = i;
+
+	printf("ceil of lglgN:%d\n", ceil);	
+
+	return(0);
+}
